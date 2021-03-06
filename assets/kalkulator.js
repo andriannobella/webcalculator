551d0ec822b84fb86617b68ed12ba95d045f55f1
Andrian Nobella <stargon2651@gmail.com>
2021-03-06 13:26:29 +0700

    Menerapkan localStorage pada Kalkulator

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -41,23 +41,32 @@ function handleOperator(operator) {
         alert('Operator sudah ditetapkan')
     }
 }
- 
+
+
 function performCalculation() {
     if (calculator.firstNumber == null || calculator.operator == null) {
         alert("Anda belum menetapkan operator");
         return;
     }
- 
+  
     let result = 0;
     if (calculator.operator === "+") {
         result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
     } else {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber)
     }
- 
+  
+    // objek yang akan dikirimkan sebagai argumen fungsi putHistory()
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    }
+    putHistory(history);
     calculator.displayNumber = result;
-}
- 
+    renderHistory();
+ }
  
 const buttons = document.querySelectorAll(".button");
 for (let button of buttons) {
